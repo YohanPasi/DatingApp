b1f5f5d67dd62da6604430be4e2b93d74e63d01b
Yohan Nishshanke <YohanN@maliban.com>
2025-01-20 10:39:00 +0530

    client side start

--- server/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ server/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270951225ce2cdceb3c8525f0e7690ce6d3216c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0dc160c0f9a9d1f070d34a8f199a2bbe30c901")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
